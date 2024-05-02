697e6aecd27fee289fe01264209650708bbe285a
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-05-02 22:56:28 +0900

    [Gold IV] Title: 최단경로, Time: 560 ms, Memory: 34516 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,85 +1,86 @@
-import java.io.*;
-import java.util.*;
-
-class Node implements Comparable<Node>{
-    int end, weight;
-
-    public Node(int end, int weight){
-        this.end = end;
-        this.weight = weight;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        return weight - o.weight;
-    }
-}
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
 
 public class Main {
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static final int INF = 100_000_000;
-    static int v,e,k;
-    static List<Node>[] list;
-    static int[] dist;
+    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        StringBuilder stringBuilder = new StringBuilder();
+        int V = read();
+        int E = read();
+        int K = read();
+        int[] dist = new int[V + 1];
+        boolean[] complete = new boolean[V + 1];
+        List<Node>[] matrix = new ArrayList[V + 1];
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(new Node(K,0));
 
 
-    public static void main(String[] args) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        v = Integer.parseInt(st.nextToken());
-        e = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(br.readLine());
-        list = new ArrayList[v + 1];
-        dist = new int[v + 1];
-
-        Arrays.fill(dist, INF);
-
-        for(int i = 1; i <= v; i++){
-            list[i] = new ArrayList<>();
+        Arrays.fill(dist, 100000000);
+        for (int i = 1; i <= V; i++) {
+            matrix[i] = new ArrayList<>();
         }
-        // 리스트에 그래프 정보를 초기화
-        for(int i = 0 ; i < e; i++){
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            // start에서 end로 가는 weight 가중치
-            list[start].add(new Node(end, weight));
+        dist[K] = 0;
+
+        for (int i = 0; i < E; i++) {
+            int u = read();
+            int v = read();
+            int w = read();
+            matrix[u].add(new Node(v,w));
         }
 
-        StringBuilder sb = new StringBuilder();
-        // 다익스트라 알고리즘
-        dijkstra(k);
-        // 출력 부분
-        for(int i = 1; i <= v; i++){
-            if(dist[i] == INF) sb.append("INF\n");
-            else sb.append(dist[i] + "\n");
+        while (!priorityQueue.isEmpty()) {
+            Node node = priorityQueue.poll();
+
+            if(complete[node.node]) continue;
+            complete[node.node] = true;
+
+            for (Node n : matrix[node.node]) {
+                int d = dist[node.node] + n.dist;
+                if (dist[n.node] > d) {
+                    dist[n.node] = d;
+                    priorityQueue.add(new Node(n.node, dist[n.node]));
+                }
+            }
         }
 
-        bw.write(sb.toString());
-        bw.close();
-        br.close();
+        for (int i = 1; i <= V; i ++) {
+            if (dist[i] == 100000000) {
+                stringBuilder.append("INF\n");
+                continue;
+            }
+            stringBuilder.append(dist[i]).append("\n");
+        }
+
+        System.out.println(stringBuilder);
+        bufferedReader.close();
     }
 
-    private static void dijkstra(int start){
-       PriorityQueue<Node> queue = new PriorityQueue<>();
-       boolean[] check = new boolean[v + 1];
-       queue.add(new Node(start, 0));
-       dist[start] = 0;
+    static int read() throws Exception {
+        int res = 0;
+        while (true) {
+            int pointer = bufferedReader.read();
+            if (pointer == 32 || pointer == 10) {
+                return res;
+            }
+            res = 10 * res + pointer - 48;
+        }
+    }
 
-       while(!queue.isEmpty()){
-           Node curNode = queue.poll();
-           int cur = curNode.end;
+    public static class Node implements Comparable<Node> {
+        private int node;
+        private int dist;
+        public Node(int node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        }
 
-           if(check[cur] == true) continue;
-           check[cur] = true;
-
-           for(Node node : list[cur]){
-               if(dist[node.end] > dist[cur] + node.weight){
-                   dist[node.end] = dist[cur] + node.weight;
-                   queue.add(new Node(node.end, dist[node.end]));
-               }
-           }
-       }
+        @Override
+        public int compareTo(Node o) {
+            return dist - o.dist;
+        }
     }
 }
