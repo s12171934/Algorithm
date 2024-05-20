88110f090e03426a8698417ebe458ae22cc0cd6b
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-05-20 16:42:23 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 344 ms, Memory: 55804 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -1,31 +1,31 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static boolean[][] draw;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static char[][] draw;
     public static void main(String[] args) throws Exception {
         int N = Integer.parseInt(br.readLine());
-        draw = new boolean[N][N * 2 - 1];
+        draw = new char[N][N * 2 - 1];
+        for(char[] s : draw) Arrays.fill(s, ' ');
         int times = (int)(Math.log(N / 3) / Math.log(2));
         fractal(0, 0, times);
 
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N * 2 -1; j++) {
-                sb.append(draw[i][j] ? "*" : " ");
-            }
-            sb.append("\n");
+        for(char[] s : draw) {
+            bw.write(s);
+            bw.write("\n");
         }
-
-        System.out.println(sb);
+        bw.flush();
+        bw.close();
         br.close();
     }
 
     public static void fractal(int y, int x, int times) {
         if(times == 0) {
-            draw[y][x + 2] = true;
-            for(int i : new int[]{1,3}) draw[y + 1][x + i] = true;
-            for(int i = 0; i < 5; i++) draw[y + 2][x + i] = true;
+            draw[y][x + 2] = '*';
+            for(int i : new int[]{1,3}) draw[y + 1][x + i] = '*';
+            for(int i = 0; i < 5; i++) draw[y + 2][x + i] = '*';
             return;
         }
 
