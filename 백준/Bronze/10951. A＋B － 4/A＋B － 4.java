e890f6ced4df520134fce2a98255a102abb60460
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-05-23 01:31:32 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String str = bufferedReader.readLine();
+            
+            if (str == null || str.isEmpty()){
+                break;
+            }
+            
+            int a = Integer.parseInt(str.split(" ")[0]);
+            int b = Integer.parseInt(str.split(" ")[1]);
+
+            System.out.println(a + b);
+        }
+
+        bufferedReader.close();
+    }
+}
