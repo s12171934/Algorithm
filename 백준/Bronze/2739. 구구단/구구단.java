e8391423a9f3250b2dd50c28d8a40ae03284faea
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-05-23 01:29:26 +0900

    [Bronze V] Title: 구구단, Time: 156 ms, Memory: 16156 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine());
+        bufferedReader.close();
+        
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(n + " * " + i + " = " + n * i);
+        }
+    }
+}
