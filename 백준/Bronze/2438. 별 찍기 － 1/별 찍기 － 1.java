97a7ae83e5f347bc495323130f2a0dd053af0b89
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-05-23 01:28:30 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 224 ms, Memory: 14960 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str = bufferedReader.readLine();
+        bufferedReader.close();
+
+        int n = Integer.parseInt(str);
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
