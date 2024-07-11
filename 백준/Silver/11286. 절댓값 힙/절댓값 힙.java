994cb05a4dcfec6d322bc5a5ec4c7e721e63b5a4
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-07-11 12:18:06 +0900

    [Silver I] Title: 절댓값 힙, Time: 280 ms, Memory: 25032 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int[] minHeap = new int[n + 1];
+        int heapSize = 0;
+
+        for (int i = 0; i < n; i++) {
+            int m = Integer.parseInt(bufferedReader.readLine());
+
+            if (m == 0) {
+                stringBuilder.append(minHeap[1]).append("\n");
+                if (minHeap[1] == 0) {
+                    continue;
+                }
+                minHeap[1] = minHeap[heapSize];
+                minHeap[heapSize] = 0;
+                heapSize--;
+
+                int idx = 1;
+                while (idx * 2 <= heapSize) {
+                    int leftOrRight = idx * 2;
+                    if (idx * 2 + 1 <= heapSize 
+                    	&& (Math.abs(minHeap[idx * 2]) > Math.abs(minHeap[idx * 2 + 1])
+                    		|| (Math.abs(minHeap[idx * 2]) == Math.abs(minHeap[idx * 2 + 1]) 
+                    			&& minHeap[idx * 2] > minHeap[idx * 2 + 1]
+                    			)
+                    		)
+                    	) {
+                        leftOrRight += 1;
+                    }
+                    if (Math.abs(minHeap[idx]) > Math.abs(minHeap[leftOrRight])
+                    		|| (Math.abs(minHeap[idx]) == Math.abs(minHeap[leftOrRight]) 
+                			&& minHeap[idx] > minHeap[leftOrRight]
+                			)
+                		) {
+                        int temp = minHeap[idx];
+                        minHeap[idx] = minHeap[leftOrRight];
+                        minHeap[leftOrRight] = temp;
+                    }
+
+                    idx = leftOrRight;
+                }
+
+
+            }
+            else {
+                heapSize++;
+                minHeap[heapSize] = m;
+                int idx = heapSize;
+                while (idx > 1
+                		&& (Math.abs(minHeap[idx/2]) > Math.abs(minHeap[idx])
+                        		|| (Math.abs(minHeap[idx/2]) == Math.abs(minHeap[idx]) 
+                        			&& minHeap[idx/2] > minHeap[idx]
+                        			)
+                        		)
+                        	) {
+                    int temp = minHeap[idx];
+                    minHeap[idx] = minHeap[idx/2];
+                    minHeap[idx/2] = temp;
+                    idx /= 2;
+                }
+            }
+        }
+
+        System.out.println(stringBuilder);
+
+        bufferedReader.close();
+    }
+}
