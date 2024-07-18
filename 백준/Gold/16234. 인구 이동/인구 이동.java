f41cd583562896ea33c6ff1815ac62b5d94f8ebe
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-07-18 16:26:43 +0900

    [Gold IV] Title: 인구 이동, Time: 792 ms, Memory: 299484 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -4,15 +4,18 @@ import java.util.*;
 public class Main {
 	
 	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static boolean[][] visited;
+	public static int[][] matrix;
+	public static int N,L,R;
+	
+	public static ArrayList<ArrayList<Node>> union;
 	
 	public static void main(String[] args) throws IOException {
-		int N = read();
-		int L = read();
-		int R = read();
+		N = read();
+		L = read();
+		R = read();
 		
-		int[][] deltas = {{1,0},{-1,0},{0,1},{0,-1}};
-		
-		int[][] matrix = new int[N][N];
+		matrix = new int[N][N];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				matrix[i][j] = read();
@@ -22,60 +25,73 @@ public class Main {
 		int count = 0;
 		
 		while (true) {
-			ArrayList<ArrayList<Node>> union = new ArrayList<>();
-			boolean finish = true;
+			union = new ArrayList<>();
+			visited = new boolean[N][N];
 			
-			boolean[][] visited = new boolean[N][N];
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					if(visited[i][j]) continue;
-					Queue<Node> q = new LinkedList<>();
-					union.add(new ArrayList<>());
-					
-					Node start = new Node(i,j);
-					q.add(start);
-					union.get(union.size() - 1).add(start);
-					visited[i][j] = true;
-					
-					while (!q.isEmpty()) {
-						Node node = q.poll();
-						
-						for (int[] d : deltas) {
-							int x = node.x + d[0];
-							int y = node.y + d[1];
-							if (x < 0 || x>= N || y < 0 || y >= N) continue;
-							if (visited[x][y]) continue;
-							int diff = Math.abs(matrix[node.x][node.y] - matrix[x][y]);
-							if (diff < L || diff > R) continue;
-							visited[x][y] = true;
-							Node unionNode = new Node(x,y);
-							q.add(unionNode);
-							union.get(union.size() - 1).add(unionNode);
-						}
-					}	
+					BFS(new Node(i,j));
 				}
 			}
 			
-			for (ArrayList<Node> nodes : union) {
-				if (nodes.size() == 1) continue;
-				finish = false;
-				int sum = 0;
-				for (Node node : nodes) {
-					sum += matrix[node.x][node.y];
-				}
-				sum /= nodes.size();
-				for (Node node : nodes) {
-					matrix[node.x][node.y] = sum;
-				}
-			}
-			
-			if (finish) break;
+			if (movePeople()) break;
 			else count++;
 		}
  		
 		System.out.println(count);
 	}
 	
+	public static void BFS(Node start) {
+		if(visited[start.x][start.y]) return;
+		
+		int[][] deltas = {{1,0},{-1,0},{0,1},{0,-1}};
+		Queue<Node> q = new LinkedList<>();
+		union.add(new ArrayList<>());
+		
+		q.add(start);
+		union.get(union.size() - 1).add(start);
+		visited[start.x][start.y] = true;
+		
+		while (!q.isEmpty()) {
+			Node node = q.poll();
+			
+			for (int[] d : deltas) {
+				int x = node.x + d[0];
+				int y = node.y + d[1];
+				
+				if (x < 0 || x>= N || y < 0 || y >= N) continue;
+				if (visited[x][y]) continue;
+				int diff = Math.abs(matrix[node.x][node.y] - matrix[x][y]);
+				if (diff < L || diff > R) continue;
+				
+				visited[x][y] = true;
+				Node unionNode = new Node(x,y);
+				q.add(unionNode);
+				union.get(union.size() - 1).add(unionNode);
+			}
+		}	
+	}
+	
+	public static boolean movePeople() {
+		boolean finish = true;
+		
+		for (ArrayList<Node> nodes : union) {
+			if (nodes.size() == 1) continue;
+			finish = false;
+			int sum = 0;
+			
+			for (Node node : nodes) {
+				sum += matrix[node.x][node.y];
+			}
+			sum /= nodes.size();
+			
+			for (Node node : nodes) {
+				matrix[node.x][node.y] = sum;
+			}
+		}
+		
+		return finish;
+	}
 	public static class Node {
 		int x,y;
 		
