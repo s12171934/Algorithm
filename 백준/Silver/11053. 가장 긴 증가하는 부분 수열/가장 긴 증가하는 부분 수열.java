d053ca1f9e6ceeb487e9d81293bb587120817f54
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-08-01 16:39:37 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 80 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,50 +1,42 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
-public class Main {
-    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args) throws Exception {
-        int N = read();
-        int[] arr = new int[N];
-        int[] dp = new int[N];
-        int result = 0;
-
-        for (int i = 0; i < N; i++) {
-            arr[i] = read();
-        }
-
-        dp[0] = arr[0];
-
-        for (int i = 1; i < N; i++) {
-            int pointer = 0;
-            while (true) {
-                if(dp[pointer] == 0) {
-                    dp[pointer] = arr[i];
-                    result = pointer;
-                    break;
-                }
-                if(dp[pointer] < arr[i]) {
-                    pointer++;
-                }
-                else {
-                    dp[pointer] = Math.min(dp[pointer], arr[i]);
-                    break;
-                }
-            }
-        }
-
-        System.out.println(result + 1);
-        bufferedReader.close();
-    }
-
-    static int read() throws Exception {
-        int res = 0;
-        while (true) {
-            int pointer = bufferedReader.read();
-            if (pointer == 32 || pointer == 10) {
-                return res;
-            }
-            res = 10 * res + pointer - 48;
-        }
-    }
+class Main{
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String args[]) throws IOException {
+		int pointer = 1;
+		int N = read();
+		int[] dp = new int[N + 1];
+		
+		for (int i = 0; i < N; i++) {
+			int cur = read();
+			if(dp[pointer - 1] < cur) {
+				dp[pointer++] = cur;
+			}
+			else {
+				int temp = pointer - 1;
+				while (true) {
+					if(dp[temp -1] < cur) {
+						dp[temp] = cur;
+						break;
+					}
+					else {
+						temp--;
+					}
+				}
+			}
+		}
+		
+		System.out.println(pointer - 1);
+	}
+	
+	static int read() throws IOException {
+		int res = 0;
+		while (true) {
+			int r = br.read();
+			if (r == 10 || r == 32) return res;
+			if (r == 13) continue;
+			res = 10 * res + (r - 48);
+		}
+	}
 }
