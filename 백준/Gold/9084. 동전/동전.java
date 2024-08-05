d57ee67565a4650959794bbdf129348181a5540b
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-08-05 22:36:14 +0900

    [Gold V] Title: 동전, Time: 72 ms, Memory: 11652 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int TC, N, M, i, j, t;
+	static int[] dp, list;
+	
+	public static void main(String[] args) throws IOException {
+		TC = Integer.parseInt(br.readLine());
+		for (t = 0; t < TC; t++) {
+			N = Integer.parseInt(br.readLine());
+			
+			list = new int[N];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (i = 0; i < N; i++) list[i] = Integer.parseInt(st.nextToken());
+			
+			M = Integer.parseInt(br.readLine());
+			dp = new int[M + 1];
+			
+			for (i = 0; i < N; i++) {
+				if (M < list[i]) break;
+				dp[list[i]] += 1;
+				for (j = list[i] + 1; j <= M; j++) { 
+					dp[j] += dp[j - list[i]];
+				}
+			}
+			
+			System.out.println(dp[M]);
+		}
+	}
+}
