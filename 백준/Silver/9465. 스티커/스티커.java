a142324245ff645a3a36e0c7cc2acd1d02492fcd
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-08-06 11:02:13 +0900

    [Silver I] Title: 스티커, Time: 308 ms, Memory: 29820 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,45 +1,44 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
-    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args) throws Exception {
-        StringBuilder stringBuilder = new StringBuilder();
-        int T = read();
-        for (int i = 0; i < T; i++) {
-            stringBuilder.append(sticker()).append("\n");
-        }
-        System.out.println(stringBuilder);
-        bufferedReader.close();
-    }
-
-    static int read() throws Exception {
-        int res = 0;
-        while (true) {
-            int pointer = bufferedReader.read();
-            if (pointer == 32 || pointer == 10) {
-                return res;
-            }
-            res = 10 * res + pointer - 48;
-        }
-    }
-
-    static int sticker() throws Exception {
-        int N = read();
-        int[][] arr = new int[N + 1][2];
-        for (int i = 0; i < 2; i++) {
-            for (int j = 1; j <= N; j++) {
-                arr[j][i] = read();
-            }
-        }
-
-        for (int i = 1; i <= N; i++) {
-            if (i == 1) continue;
-            for (int j = 0; j < 2; j++) {
-                arr[i][Math.abs(j - 1)] += Math.max(arr[i - 1][j], arr[i - 2][j]);
-            }
-        }
-
-        return Math.max(arr[N][0], arr[N][1]);
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		int TC = read();
+		
+		for (int tc = 0; tc < TC; tc++) {
+			int n = read();
+			int[][] list = new int[2][n + 1];
+			int[][] dp = new int[3][n + 1];
+			for (int i = 0; i < 2; i++) {
+				for (int j = 1; j <= n; j++) {
+					list[i][j] = read();
+				}
+			}
+			dp[0][1] = list[0][1];
+			dp[1][1] = list[1][1];
+			dp[2][1] = Math.max(dp[0][1], dp[1][1]);
+			
+			for (int i = 2; i <= n; i++) {
+				dp[0][i] =  Math.max(dp[2][i - 2], dp[1][i - 1]) + list[0][i];
+				dp[1][i] =  Math.max(dp[2][i - 2], dp[0][i - 1]) + list[1][i];
+				dp[2][i] = Math.max(dp[0][i], dp[1][i]);
+			}
+			
+			sb.append(dp[2][n]).append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+	
+	static int read() throws IOException {
+		int res = 0;
+		while (true) {
+			int r = br.read();
+			if (r == 10 || r == 32) return res;
+			if (r == 13) continue;
+			res = 10 * res + (r - 48);
+		}
+	}
 }
