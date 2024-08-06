7cd24cdabe7e3eee49b8e5c46d601c0efa97249f
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-08-06 11:48:45 +0900

    [Silver I] Title: 스티커, Time: 244 ms, Memory: 20416 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -10,23 +10,18 @@ public class Main {
 		for (int tc = 0; tc < TC; tc++) {
 			int n = read();
 			int[][] list = new int[2][n + 1];
-			int[][] dp = new int[3][n + 1];
 			for (int i = 0; i < 2; i++) {
 				for (int j = 1; j <= n; j++) {
 					list[i][j] = read();
 				}
 			}
-			dp[0][1] = list[0][1];
-			dp[1][1] = list[1][1];
-			dp[2][1] = Math.max(dp[0][1], dp[1][1]);
 			
 			for (int i = 2; i <= n; i++) {
-				dp[0][i] =  Math.max(dp[2][i - 2], dp[1][i - 1]) + list[0][i];
-				dp[1][i] =  Math.max(dp[2][i - 2], dp[0][i - 1]) + list[1][i];
-				dp[2][i] = Math.max(dp[0][i], dp[1][i]);
+				list[0][i] +=  Math.max(list[1][i - 2], list[1][i - 1]);
+				list[1][i] +=  Math.max(list[0][i - 2], list[0][i - 1]);
 			}
 			
-			sb.append(dp[2][n]).append("\n");
+			sb.append(Math.max(list[0][n], list[1][n])).append("\n");
 		}
 		
 		System.out.println(sb.toString());
