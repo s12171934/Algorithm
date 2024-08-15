351e77013f1f02d41983993895041e16931362e0
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-08-15 23:16:02 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -1,13 +1,14 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws IOException {
 		int[] dice = new int[7];
 		int[][][] move = setMoveMatrix();
 		int[][] deltas = {{},{0,1},{0,-1},{-1,0},{1,0}};
+		//초기정보 저장
 		int N = read();
 		int M = read();
 		int x = read();
@@ -25,9 +26,12 @@ public class Main {
 		
 		for (int k = 0; k < K; k++) {
 			int d = read();
+			//이동불가 명령 무시
 			if (x + deltas[d][0] < 0 || x + deltas[d][0] >= N || y + deltas[d][1] < 0 || y + deltas[d][1] >= M) continue;
 			x += deltas[d][0];
 			y += deltas[d][1];
+
+			//주사위 이동
 			int temp = u;
 			u = move[u][f][d];
 			if(d == 3) {
@@ -36,9 +40,8 @@ public class Main {
 			else if (d == 4) {
 				f = 7 - temp;
 			}
-			
-			System.out.println(dice[7 - u]);
-			
+		
+			//지도 및 주사위 갱신
 			if (matrix[x][y] == 0) {
 				matrix[x][y] = dice[u];
 			}
@@ -46,13 +49,18 @@ public class Main {
 				dice[u] = matrix[x][y];
 				matrix[x][y] = 0;
 			}
+			
+			//출력
+			sb.append(dice[7 - u]).append("\n");
 		}
-		
+		System.out.println(sb);
 	}
 	
+	//주자위의 아랫 면과 남쪽 면이 주어졌을때, 동서남북 이동 시 아랫 면이 되는 방법을 모두 저장한 3차원 배열 출력
 	static int[][][] setMoveMatrix() {
 		int[][][] move = new int[7][7][5];
 		
+		//아랫 면과 남쪽면이 주어졌을때의 동쪽면
 		int[][] east = {
 				{},
 				{0,0,3,5,2,4,0},
@@ -63,6 +71,7 @@ public class Main {
 				{0,0,4,2,5,3,0},
 		};
 		
+		//서쪽과 북쪽 연산하여 저장
 		for (int i = 1; i <= 6; i++) {
 			for (int j = 1; j <= 6; j++) {
 				if (i == j || i + j == 7) continue;
