ea9e7d79010b61e157103f52adccee234ca71687
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-08-16 13:13:47 +0900

    [Silver II] Title: DNA 비밀번호, Time: 192 ms, Memory: 20484 KB -BaekjoonHub

---
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -0,0 +1,53 @@
+import java.io.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int cnt;
+	
+	public static void main(String[] args) throws IOException {
+		int S = read();
+		int P = read();
+		String dna = br.readLine();
+		int[] list = {-1 * read(), -1 * read(), -1 * read(), -1 * read()};
+		for(int i = 0; i < P; i++) {
+			list[findIdx(dna.charAt(i))]++;
+		}
+		check(list);
+		for(int i = P; i < S; i++) {
+			list[findIdx(dna.charAt(i - P))]--;
+			list[findIdx(dna.charAt(i))]++;
+			check(list);
+		}
+		System.out.println(cnt);
+	}
+	
+	static void check(int[] list) {
+		for(int i : list) {
+			if(i < 0) return;
+		}
+		cnt++;
+	}
+	
+	static int findIdx(char c) {
+		switch(c) {
+		case 'A' :
+			return 0;
+		case 'C' :
+			return 1;
+		case 'G' :
+			return 2;
+		default:
+			return 3;
+		}
+	}
+	
+	static int read() throws IOException {
+		int res = 0;
+		while (true) {
+			int r = br.read();
+			if (r == 10 || r == 32) return res;
+			if (r == 13) continue;
+			res = 10 * res + (r - 48);
+		}
+	}
+}
