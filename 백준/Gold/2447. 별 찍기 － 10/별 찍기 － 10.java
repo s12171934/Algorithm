a1df0d82cb96cfce6292e7a73fca1382740969a3
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-08-19 20:03:55 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 240 ms, Memory: 70328 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+		int K = (int)Math.round(Math.log(N) / Math.log(3));
+		char[][][] matrix = new char[K + 1][][];
+		matrix[0] = new char[][] {{'*'}};
+		for (int n = 1; n <= K; n++) {
+			int lastLen = matrix[n - 1].length;
+			matrix[n] = new char[lastLen * 3][lastLen * 3];
+			for (int i = 0; i < matrix[n].length; i++) {
+				Arrays.fill(matrix[n][i], ' ');
+			}
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 3; j++) {
+					if (i == 1 && j == 1) continue;
+					for (int k = 0; k < lastLen; k++) {
+						for (int l = 0; l < lastLen; l++) {
+							matrix[n][i * lastLen + k][j * lastLen + l] = matrix[n - 1][k][l];
+						}
+					}
+				}
+			}
+		}
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append(matrix[K][i][j]);
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
