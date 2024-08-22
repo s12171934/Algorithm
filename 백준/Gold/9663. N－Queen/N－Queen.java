7b9fcf0cbf41337a05d163e99b3a70c3d3a28f62
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-08-22 11:41:05 +0900

    [Gold IV] Title: N-Queen, Time: 5532 ms, Memory: 12304 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,37 +1,28 @@
 import java.io.*;
 
 public class Main {
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static int N;
-    private static int[] arr;
-    private static int res = 0;
-    public static void main(String[] args) throws Exception {
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        nQueen(0);
-        System.out.println(res);
-        br.close();
-    }
-
-    public static void nQueen(int n) {
-        if (n == N) {
-            res++;
-            return;
-        }
-        for (int i = 0; i < N; i++) {
-            arr[n] = i;
-            boolean check = true;
-            for (int j = 0; j < n; j++){
-                if(arr[j] == i
-                || arr[j] + n - j == i
-                || arr[j] - n + j == i) {
-                    check = false;
-                    break;
-                }
-            }
-            if (check) {
-                nQueen(n + 1);
-            }
-        }
-    }
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static int N , cnt;
+	
+	public static void main(String[] args) throws IOException {
+		N = Integer.parseInt(br.readLine());
+		NQueen(1, 0);
+		System.out.println(cnt);
+	}
+	
+	static void NQueen(int idx, long map) {
+		if(idx > N) {
+			cnt++;
+			return;
+		}
+		loop: for (int i = 0; i < N; i++) {
+			if((map & (15l << (i * 4))) != 0) continue loop;
+			for (int j = 0; j < N; j++) {
+				long idx2 = ((map & (15l << (j * 4))) >> (j * 4));
+				if(idx2 == 0) continue;
+				if(Math.abs(idx2 - idx) == Math.abs(i - j)) continue loop;
+			}
+			NQueen(idx + 1, map | (((long)idx) << (i * 4)));
+		}
+	}
 }
