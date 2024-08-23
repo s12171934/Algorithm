38cb9080956fb25d4069eddeae9a68413da49f27
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-08-23 15:18:22 +0900

    [Silver I] Title: 경로 찾기, Time: 108 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,57 +1,55 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static int result[][], N;
+	static ArrayList<Integer>[] graph;
 	
-	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+	
 	public static void main(String[] args) throws IOException {
-		
-		int n = readInt();
-		StringBuilder sb = new StringBuilder();
-		
-		ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-		for (int i = 0; i < n; i++) {
-			graph.add(new ArrayList<>());
-			for (int j = 0; j < n; j++) {
-				if(readInt() == 1) {
-					graph.get(i).add(j);
+		N = read();
+		graph = new ArrayList[N];
+		for (int i = 0; i < N; i++) graph[i] = new ArrayList<>();
+		result = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++){
+				if(read() == 1) graph[i].add(j);
+			}
+ 		}
+		for (int i = 0; i < N; i++) {
+			Queue<Integer> q = new ArrayDeque<>();
+			q.add(i);
+			while(!q.isEmpty()) {
+				int cur = q.poll();
+				for(int next : graph[cur]) {
+					if(next == i) {
+						result[i][i] = 1;
+						continue;
+					}
+					if(result[i][next] == 1) continue;
+					result[i][next] = 1;
+					q.add(next);
 				}
 			}
 		}
-		
-		for (int i = 0; i < n; i++) {
-			int[] list = new int[n];
-			Queue<Integer> queue = new LinkedList<>();
-			queue.add(i);
-			
-			while (!queue.isEmpty()) {
-				int node = queue.poll();
-				
-				for (int n1 : graph.get(node)) {
-					if (list[n1] == 1) continue;
-					list[n1] = 1;
-					queue.add(n1);
-				}
-			}
-			
-			for (int l : list) {
-				sb.append(l).append(" ");
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				sb.append(result[i][j]).append(" ");
 			}
 			sb.append("\n");
 		}
-	
-		System.out.println(sb);
+		System.out.println(sb.toString());
 	}
 	
-	public static int readInt() throws IOException {
+	static int read() throws IOException {
 		int res = 0;
 		while (true) {
 			int r = br.read();
-			if (r == 32 || r == 10) return res;
+			if (r == 10 || r == 32) return res;
 			if (r == 13) continue;
 			res = 10 * res + r - 48;
 		}
 	}
-
 }
