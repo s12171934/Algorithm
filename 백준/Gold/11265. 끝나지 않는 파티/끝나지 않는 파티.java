6794b36a6ab5bfcbc1dd54bd9e16d3f2d7894ed7
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-08-23 15:35:20 +0900

    [Gold V] Title: 끝나지 않는 파티, Time: 308 ms, Memory: 25940 KB -BaekjoonHub

---
+++ 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder sb = new StringBuilder();
+	static StringTokenizer st;
+	
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] matrix = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				matrix[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for (int k = 0; k < N; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+				}
+			}
+		}
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken()) - 1;
+			int e = Integer.parseInt(st.nextToken()) - 1;
+			int t = Integer.parseInt(st.nextToken());
+			sb.append(matrix[s][e] <= t ? "Enjoy other party\n" : "Stay here\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
