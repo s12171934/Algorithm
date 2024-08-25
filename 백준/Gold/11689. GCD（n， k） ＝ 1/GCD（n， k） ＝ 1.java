a6b03d3399bab916be4b57d33547559ef8c41356
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-08-25 11:02:32 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 96 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+		long N = Long.parseLong(br.readLine());
+		long res = 1;
+		if(N == 1) {
+			System.out.println(1);
+			return;
+		}
+		boolean[] prime = new boolean[(int)Math.sqrt(N) + 1];
+		prime[0] = true;
+		prime[1] = true;
+		for (int i = 2; i < prime.length; i++) {
+			if(prime[i]) continue;
+			int cnt = 1;
+			while(i * cnt < prime.length) {
+				prime[i * cnt++] = true;
+			}
+			long exp = 0;
+			while (N % i == 0) {
+				N /= i;
+				exp++;
+			}
+			if(exp == 0) continue;
+			res *= (int)(Math.pow(i, exp) - Math.pow(i, exp - 1));
+			if(N == 1) break;
+		}
+		if(N != 1) {
+			res *= (N - 1);
+		}
+		
+		System.out.println(res);
+	}
+}
