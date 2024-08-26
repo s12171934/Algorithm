52c5eb240e6f67019382ec88312411ed3943e34d
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-08-26 23:56:58 +0900

    [Bronze II] Title: 분산처리, Time: 4 ms, Memory: 856 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.go
+++ 백준/Bronze/1009. 분산처리/분산처리.go
@@ -11,7 +11,20 @@ func main() {
 	sc := bufio.NewScanner(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
 	defer writer.Flush()
-	
+
+	list := [][]int{
+		{10},
+		{1},
+		{2,4,8,6},
+		{3,9,7,1},
+		{4,6},
+		{5},
+		{6},
+		{7,9,3,1},
+		{8,4,2,6},
+		{9,1},
+	}
+
 	sc.Scan()
 	TC, _ := strconv.Atoi(sc.Text())
 	
@@ -20,14 +33,7 @@ func main() {
 		strNums := strings.Split(strings.TrimSpace(sc.Text()), " ")
 		a, _ := strconv.Atoi(strNums[0])
 		b, _ := strconv.Atoi(strNums[1])
-		res := 1;
-		for i := 0; i < b; i++ {
-			res *= a
-			res %= 10
-		}
-		if res == 0 {
-			res = 10
-		}
+		res := list[a % 10][(b - 1) % len(list[a % 10])]
 		writer.Write([]byte(strconv.Itoa(res) + "\n"))
 	}
 }
