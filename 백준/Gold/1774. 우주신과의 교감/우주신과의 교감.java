b336840ccd6dfd489091a227accf0dca2cc54c16
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-09-02 17:11:12 +0900

    [Gold III] Title: 우주신과의 교감, Time: 168 ms, Memory: 34960 KB -BaekjoonHub

--- 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
+++ 백준/Gold/1774. 우주신과의 교감/우주신과의 교감.java
@@ -5,7 +5,7 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
-	static int[] parents;
+	static int parents[], cnt;
 	
 	static int read() throws Exception {
 		if (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
@@ -17,11 +17,14 @@ public class Main {
 		return parents[n];
 	}
 	
-	static void union(int a, int b) {
+	static boolean union(int a, int b) {
+		if (getParent(a) == getParent(b)) return false;
 		a = getParent(a);
 		b = getParent(b);
 		if (a < b) parents[b] = a;
 		else parents[a] = b;
+		cnt--;
+		return true;
 	}
 	
 	static class Node implements Comparable<Node> {
@@ -43,32 +46,26 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		int N = read();
 		int M = read();
+		cnt = N - 1;
+		double sum = 0.0;
+		
 		int[][] input = new int[N][2];
 		for (int i = 0; i < 2 * N; i++) input[i / 2][i % 2] = read();
+		
 		Queue<Node> pq = new PriorityQueue<>();
 		for (int i = 0; i < N; i++) {
 			for (int j = i + 1; j < N; j++) {
 				pq.add(new Node(i + 1,j + 1,Math.sqrt(Math.pow(input[i][0] - input[j][0], 2) + Math.pow(input[i][1] - input[j][1], 2))));			
 			}
 		}
-		int cnt = N - 1;
+		
 		parents = new int[N + 1];
 		for (int i = 1; i <= N; i++) parents[i] = i;
-		for (int i = 0; i < M; i++) {
-			int n1 = read();
-			int n2 = read();
-			if (getParent(n1) == getParent(n2)) continue;
-			union(n1, n2);
-			cnt--;
-		}
+		for (int i = 0; i < M; i++) union(read(), read());
 		
-		double sum = 0.0;
 		while (cnt > 0) {
 			Node cur = pq.poll();
-			if (getParent(cur.n1) == getParent(cur.n2)) continue;
-			union(cur.n1, cur.n2);
-			sum += cur.w;
-			cnt--;
+			if (union(cur.n1, cur.n2)) sum += cur.w;
 		}
 		
 		System.out.printf("%.2f", sum);
