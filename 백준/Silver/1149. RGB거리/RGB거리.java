3735284ee7b94c802b724e0851fbb3780b0daaae
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-09-04 10:38:17 +0900

    [Silver I] Title: RGB거리, Time: 68 ms, Memory: 12036 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,23 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util. *;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        int [][] rgb = new int[3][N + 1];
-
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder res = new StringBuilder();
+    static StringTokenizer st;
+    
+    static int read() throws Exception {
+    	if (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+    	return Integer.parseInt(st.nextToken());
+    }
+     
+    public static void main(String[] args) throws Exception {
+        int N = read();
+        int[][] dp = new int[2][3];
         for (int i = 1; i <= N; i++) {
-            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            rgb[0][i] = Math.min(rgb[1][i - 1], rgb[2][i - 1]) + Integer.parseInt(stringTokenizer.nextToken());
-            rgb[1][i] = Math.min(rgb[0][i - 1], rgb[2][i - 1]) + Integer.parseInt(stringTokenizer.nextToken());
-            rgb[2][i] = Math.min(rgb[1][i - 1], rgb[0][i - 1]) + Integer.parseInt(stringTokenizer.nextToken());
+        	for (int j = 0; j < 3; j++) dp[i % 2][j] = read() + Math.min(dp[(i + 1) % 2][(j + 1) % 3], dp[(i + 1) % 2][(j + 2) % 3]);
         }
-
-        System.out.println(Math.min(Math.min(rgb[0][N], rgb[1][N]), rgb[2][N]));
-
-        bufferedReader.close();
+        System.out.println(Math.min(Math.min(dp[N % 2][0], dp[N % 2][1]), dp[N % 2][2]));
     }
 }
