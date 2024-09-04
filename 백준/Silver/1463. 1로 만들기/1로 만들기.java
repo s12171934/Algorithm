0fca2f3ba15389719ab6b4f1d8a356dcb7e17a25
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-09-04 10:29:42 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18640 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,32 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Main {
-    static int[] dp;
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(bufferedReader.readLine());
-        dp = new int[N + 1];
-        for (int i = 0; i < N; i++) {
-            makeOne(i + 1);
-        }
-        System.out.println(makeOne(N));
-        bufferedReader.close();
-    }
-
-    static int makeOne(int N) {
-        if (N != 1 && dp[N] == 0) {
-            if (N % 6 == 0) {
-                dp[N] = Math.min(Math.min(makeOne(N / 3), makeOne(N / 2)), makeOne(N - 1)) + 1;
-            } else if (N % 3 == 0) {
-                dp[N] = Math.min(makeOne(N / 3), makeOne(N - 1)) + 1;
-            } else if (N % 2 == 0) {
-                dp[N] = Math.min(makeOne(N / 2), makeOne(N - 1)) + 1;
-            } else {
-                dp[N] = makeOne(N - 1) + 1;
-            }
-        }
-        return dp[N];
-    }
+	static int[] dp;
+	public static void main(String[] args) {
+		int N = new java.util.Scanner(System.in).nextInt();
+		dp = new int[N + 1];
+		for (int i = 2; i <= N; i++) dp[i] = calc(i);
+		System.out.println(dp[N]);
+	}
+	static int calc(int n) {
+		if (n == 1 || dp[n] != 0) return dp[n];
+		if (n % 6 == 0) return Math.min(Math.min(calc(n / 3), calc(n / 2)), calc(n - 1)) + 1;
+		if (n % 3 == 0) return Math.min(calc(n / 3), calc(n - 1)) + 1;
+		if (n % 2 == 0) return Math.min(calc(n / 2), calc(n - 1)) + 1;
+		return calc(n - 1) + 1;
+	}
 }
