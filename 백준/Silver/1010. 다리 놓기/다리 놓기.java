121114ab7c48c377dc7b765222b6b088289036e0
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-09-04 17:37:20 +0900

    [Silver V] Title: 다리 놓기, Time: 64 ms, Memory: 11848 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -5,29 +5,22 @@ public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder sb = new StringBuilder();
-	static int[][] matrix = makeMatrix();
+	static int[][] matrix = new int[31][31];
 	
 	static int read() throws Exception {
 		if (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
 		return Integer.parseInt(st.nextToken());
 	}
 	
-	static int[][] makeMatrix() {
-		int[][] mat = new int[31][31];
-		for(int i = 1; i <= 30; i++) {
-			mat[1][i] = i;
-			mat[i][i] = 1;
-		}
+	static void makeMatrix() {
+		for(int i = 1; i <= 30; i++) {matrix[1][i] = i;matrix[i][i] = 1;}
 		for(int i = 2; i <= 30; i++) {
-			for(int j = i + 1; j <= 30; j++) {
-				mat[i][j] = mat[i - 1][j - 1] + mat[i][j - 1];
-			}
+			for(int j = i + 1; j <= 30; j++) matrix[i][j] = matrix[i][j - 1] + matrix[i - 1][j - 1];
 		}
-		return mat;
 	}
 	
 	public static void main(String[] args) throws Exception {
-		int TC = read();
+		int TC = read();makeMatrix();
 		while(TC-- > 0) sb.append(matrix[read()][read()]).append("\n");
 		System.out.println(sb.toString());
 	}
