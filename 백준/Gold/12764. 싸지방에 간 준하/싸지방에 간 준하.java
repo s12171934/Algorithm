67792465bb33af39c5b893279c813d5b7de1f9db
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-09-16 17:53:48 +0900

    [Gold III] Title: 싸지방에 간 준하, Time: 780 ms, Memory: 56464 KB -BaekjoonHub

--- 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.java
+++ 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.java
@@ -4,8 +4,7 @@ import java.util.*;
 public class Main {
    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
    static StringTokenizer st;
-   static int N, list[][];
-   static List<Integer> res = new ArrayList<>();
+   static int N, list[][], computers, used[];
 
    static int read() throws Exception {
        if (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
@@ -16,31 +15,38 @@ public class Main {
         N = read(); list = new int[N][];
         for (int i = 0; i < N; i++) list[i] = new int[]{read(), read(), 0};
         Arrays.sort(list, Comparator.comparingInt(o -> o[0]));
+        used = new int[N];
    }
 
    static void solve() {
        Queue<int[]> use = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
        Queue<Integer> wait = new PriorityQueue<>();
        use.add(list[0]);
-       res.add(1);
+       used[0] = 1;
+       computers = 1;
        for (int i = 1; i < N; i++) {
            while (!use.isEmpty() && use.peek()[1] < list[i][0]) wait.add(use.poll()[2]);
            if (wait.isEmpty()) {
-               list[i][2] = res.size();
-               res.add(1);
+               list[i][2] = computers;
+               used[computers++] = 1;
            }
            else {
                list[i][2] = wait.poll();
-               res.set(list[i][2], res.get(list[i][2]) + 1);
+               used[list[i][2]]++;
            }
            use.add(list[i]);
        }
    }
 
    static void result() {
+       int cnt = 0;
        StringBuilder sb = new StringBuilder();
-       sb.append(res.size()).append('\n');
-       for (int r : res) sb.append(r).append(' ');
+       for (int r : used) {
+           if (r == 0) break;
+           cnt++;
+           sb.append(r).append(' ');
+       }
+       System.out.println(cnt);
        System.out.println(sb);
    }
 
