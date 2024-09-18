d367d04184ec6cd292ec7e71a99faa60598a2ddb
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-09-19 00:18:02 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 132 ms, Memory: 16960 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.java
@@ -12,6 +12,7 @@ public class Main {
 		return Integer.parseInt(st.nextToken());
 	}
 
+	// 구름 이동 -> 비바라기 마법 -> 물복사버그 -> 구름 생성 (반복) 
 	static void solve(int d, int s) {
 		cloud = moveCloud(deltas[d], s);
 		rain();
@@ -19,10 +20,12 @@ public class Main {
 		cloud = makeCloud();
 	}
 
+	// 구름 이동시 경계를 만나면 순환해야하므로 나머지를 이용해 idx확인
+	// 이동량이 N보다 클 수 있으므로 미리 나머지를 구하고 N을 더하여 다시 나머지를 구함
 	static boolean[][] moveCloud(int[] d, int s) {
 		boolean[][] next = new boolean[N][N];
 		for (int i = 0; i < N * N; i++) {
-			if (cloud[i / N][i % N]) next[((i / N + d[0] * s + N) % N + N) % N][((i % N + d[1] * s + N) % N + N) % N] = true;
+			if (cloud[i / N][i % N]) next[((i / N + d[0] * s) % N + N) % N][((i % N + d[1] * s) % N + N) % N] = true;
 		}
 		return next;
 	}
@@ -60,6 +63,7 @@ public class Main {
 		N = read(); M = read(); matrix = new int[N][N];
 		for (int i = 0; i < N * N; i++) matrix[i / N][i % N] = read();
 		cloud = new boolean[N][N];
+		// 초기 구름 생성
 		cloud[N -1][0] = cloud[N -2][0] = cloud[N - 1][1] = cloud[N -2][1] = true;
 		for (int i = 0; i < M; i++) solve(read(), read());
 		System.out.println(sumWater());
