398e72b13ad8c84704b729b1c339589b5c88a260
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-09-30 13:49:16 +0900

    [Gold V] Title: 평범한 배낭, Time: 132 ms, Memory: 51308 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -2,38 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static int read() throws Exception {
-        int res = 0;
-        while (true) {
-            int n = br.read();
-            if (n == 10 || n == 32) {
-                return res;
-            }
-            res = 10 * res + n - 48;
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static StringTokenizer st;
+
+  static int read() throws Exception {
+    if (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+    return Integer.parseInt(st.nextToken());
+  }
+
+  public static void main(String[] args) throws Exception {
+    int N = read(), K = read(), matrix[][] = new int[N + 1][K + 1];
+    for (int i = 1; i <= N; i++) {
+      int V = read(), C = read();
+      for (int j = 1; j <= K; j++) {
+        if (j < V) matrix[i][j] = matrix[i - 1][j];
+        else {
+          matrix[i][j] = Math.max(matrix[i - 1][j - V] + C, matrix[i - 1][j]);
         }
+      }
     }
-
-    public static void main(String[] args) throws Exception {
-        int N = read();
-        int K = read();
-        int[][] arr = new int[N + 1][2];
-        int[][] dp = new int[N + 1][K + 1];
-
-        for (int i = 1; i <= N; i++) {
-            arr[i][0] = read();
-            arr[i][1] = read();
-        }
-
-        for (int i = 1; i <= K; i++) {
-            for (int j = 1; j <= N; j++) {
-                dp[j][i] = dp[j - 1][i];
-                if(arr[j][0] > i) continue;
-                dp[j][i] = Math.max(dp[j][i], dp[j - 1][i - arr[j][0]] + arr[j][1]);
-            }
-        }
-
-        System.out.println(dp[N][K]);
-        br.close();
-    }
+    System.out.println(matrix[N][K]);
+  }
 }
