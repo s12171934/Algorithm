0297028561b1b59824070ee40b620f93e5ccead3
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-09-30 14:21:13 +0900

    [Gold III] Title: 앱, Time: 116 ms, Memory: 17152 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -2,57 +2,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    static int[][] dp;
-    public static void main(String[] args) throws Exception {
-        int N = read();
-        int M = read();
-        ArrayList<int[]> programs = new ArrayList<>();
+  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static StringTokenizer st;
 
-        for (int i = 0; i < N; i++) {programs.add(new int[]{read(), -1});}
-        int c = 0;
-        for (int i = 0; i < N; i++) {
-            int k = read();
-            if(k == 0){
-                M -= programs.get(c)[0];
-                programs.remove(c);
-                continue;
-            }
-            programs.get(c)[1] = k;
-            c++;
-        }
-        N = programs.size();
-        dp = new int[N][N * 100 + 1];
-        int res = N * 100 + 1;
+  static int read() throws Exception {
+    if (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
+    return Integer.parseInt(st.nextToken());
+  }
+  
+  static int[][] dp;
+  public static void main(String[] args) throws Exception {
+    int N = read(), M = read();
+    ArrayList<int[]> programs = new ArrayList<>();
 
-        for (int i = 0; i < N; i++) {
-            int[] program = programs.get(i);
-
-            for (int j = 100 * N; j >= 0; j--) {
-
-                if (i == 0) {
-                    if (j < program[1]) continue;
-                    dp[i][j] = program[0];
-                }
-                else {
-                    if (j < program[1]) dp[i][j] = dp[i -1][j];
-                    else dp[i][j] = Math.max(dp[i -1][j - program[1]] + program[0], dp[i - 1][j]);
-                }
-
-                if (dp[i][j] >= M) res = Math.min(res, j);
-            }
-        }
-
-        System.out.println(res == N * 100 + 1 ? 0 : res);
-
-        bufferedReader.close();
+    for (int i = 0; i < N; i++) {programs.add(new int[]{read(), -1});}
+    int c = 0;
+    for (int i = 0; i < N; i++) {
+      int k = read();
+      if(k == 0){
+        M -= programs.get(c)[0];
+        programs.remove(c);
+        continue;
+      }
+      programs.get(c)[1] = k;
+      c++;
     }
-    public static int read() throws Exception {
-        int res = 0;
-        while (true) {
-            int n = bufferedReader.read();
-            if (n == 10 || n == 32) return res;
-            res = res * 10 + n - 48;
+    N = programs.size();
+    dp = new int[N][N * 100 + 1];
+    int res = N * 100 + 1;
+
+    for (int i = 0; i < N; i++) {
+      int[] program = programs.get(i);
+
+      for (int j = 100 * N; j >= 0; j--) {
+
+        if (i == 0) {
+          if (j < program[1]) continue;
+          dp[i][j] = program[0];
         }
+        else {
+          if (j < program[1]) dp[i][j] = dp[i -1][j];
+          else dp[i][j] = Math.max(dp[i -1][j - program[1]] + program[0], dp[i - 1][j]);
+        }
+
+        if (dp[i][j] >= M) res = Math.min(res, j);
+      }
     }
+
+    System.out.println(res == N * 100 + 1 ? 0 : res);
+  }
 }
