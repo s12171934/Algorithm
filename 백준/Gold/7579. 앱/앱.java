2f547979ad17dead612372004c586d4f9683f051
s12171934 <148848550+s12171934@users.noreply.github.com>
2024-09-30 14:25:30 +0900

    [Gold III] Title: 앱, Time: 112 ms, Memory: 16564 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -9,42 +9,30 @@ public class Main {
     if (st == null || !st.hasMoreTokens()) st = new StringTokenizer(br.readLine());
     return Integer.parseInt(st.nextToken());
   }
-  
-  static int[][] dp;
+
   public static void main(String[] args) throws Exception {
     int N = read(), M = read();
     ArrayList<int[]> programs = new ArrayList<>();
 
     for (int i = 0; i < N; i++) {programs.add(new int[]{read(), -1});}
-    int c = 0;
-    for (int i = 0; i < N; i++) {
+    for (int i = 0, c = 0; i < N; i++) {
       int k = read();
       if(k == 0){
         M -= programs.get(c)[0];
         programs.remove(c);
         continue;
       }
-      programs.get(c)[1] = k;
-      c++;
+      programs.get(c++)[1] = k;
     }
     N = programs.size();
-    dp = new int[N][N * 100 + 1];
+    int[][] dp = new int[N + 1][N * 100 + 1];
     int res = N * 100 + 1;
 
-    for (int i = 0; i < N; i++) {
-      int[] program = programs.get(i);
-
+    for (int i = 1; i <= N; i++) {
+      int[] program = programs.get(i - 1);
       for (int j = 100 * N; j >= 0; j--) {
-
-        if (i == 0) {
-          if (j < program[1]) continue;
-          dp[i][j] = program[0];
-        }
-        else {
-          if (j < program[1]) dp[i][j] = dp[i -1][j];
-          else dp[i][j] = Math.max(dp[i -1][j - program[1]] + program[0], dp[i - 1][j]);
-        }
-
+        if (j < program[1]) dp[i][j] = dp[i -1][j];
+        else dp[i][j] = Math.max(dp[i -1][j - program[1]] + program[0], dp[i - 1][j]);
         if (dp[i][j] >= M) res = Math.min(res, j);
       }
     }
